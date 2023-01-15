2aa43f5a26202a877dbc312ca6959df1284fb274
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-01-15 15:36:09 +0200

    ins

--- instructions/indexIns.js
+++ instructions/indexIns.js
@@ -5,23 +5,24 @@ function timeline() {
         if ((typeof data == "undefined") || (studySessionData.doneInstructions == "")) {
             let updatedDates = updateDates();
             studySessionData.startDate = startDate;
-            // let goIns = async function () {
-            // let doneInstructions = await startFirstDay();
-            // if (doneInstructions == "doneInstructions") {
-            studySessionData.doneInstructions = "doneInstructions";
-            studySessionData.expDaysDate = updatedDates.fullDate;
-            platform.saveSession(studySessionData, true);
-            document.getElementById("ins12").style.display = "inline";
-            document.getElementById("ins12").addEventListener("click", function () {
-                setTimeout(() => {
-                    platform.goToUrl("days/dayOne/dayOne.html");
-                }, 200)
-            });
-            //goIns();
-
-
-        } else {
-            moveToDay()
+            let goIns = async function () {
+                let doneInstructions = await startFirstDay();
+                studySessionData.doneInstructions = "stratIns";
+                if (doneInstructions == "doneInstructions") {
+                    studySessionData.doneInstructions = "doneInstructions";
+                    studySessionData.expDaysDate = updatedDates.fullDate;
+                    platform.saveSession(studySessionData, true);
+                    document.getElementById("ins12").style.display = "inline";
+                    document.getElementById("ins12").addEventListener("click", function () {
+                        setTimeout(() => {
+                            platform.goToUrl("days/dayOne/dayOne.html");
+                        }, 200)
+                    });
+                } else {
+                    moveToDay()
+                }
+            }
+            goIns();
         }
     })
 
