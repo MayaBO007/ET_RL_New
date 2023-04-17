15e85ab5abe9635d8e34bf110f5b080c2fe890c3
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-04-17 10:30:18 +0300

    ss

--- instructions/questions/Multiple-Choice-Quiz-JavaScript-master/quiz.js
+++ instructions/questions/Multiple-Choice-Quiz-JavaScript-master/quiz.js
@@ -171,6 +171,7 @@ function scoreRender() {
             let updatedDates = updateDates();
             studySessionData.doneInstructions = "doneInstructions";
             studySessionData.expDaysDate = updatedDates.fullDate;
+            studySessionData.startDate = startDate;
             platform.saveSession(studySessionData, true);
             setTimeout(() => {
                 platform.goToUrl("days/training/training.html");
