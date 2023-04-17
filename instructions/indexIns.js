3f70853e97f05030f7087e4754324ebeeae285d4
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-04-17 11:15:28 +0300

    ss

--- instructions/indexIns.js
+++ instructions/indexIns.js
@@ -4,15 +4,14 @@ function timeline() {
     platform.getAllSessions().then((data) => {
         if ((typeof data == "undefined") || (studySessionData.doneInstructions == "")) {
             let updatedDates = updateDates();
-            studySessionData.startDate = startDate;
-            deleteFromSessionData();
-            platform.saveSession(studySessionData, true);
             let goIns = async function () {
                 let doneInstructions = await startFirstDay();
                 studySessionData.doneInstructions = "stratIns";
                 if (doneInstructions == "doneInstructions") {
                     studySessionData.doneInstructions = "doneInstructions";
                     studySessionData.expDaysDate = updatedDates.fullDate;
+                    studySessionData.startDate = startDate;
+                    deleteFromSessionData();
                     platform.saveSession(studySessionData, true);
                 } else {
                     moveToDay()
