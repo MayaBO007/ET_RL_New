710877b71642bdf7778d06467ae819e65a44b62f
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-05-14 11:02:02 +0300

    ss

--- instructions/indexIns.js
+++ instructions/indexIns.js
@@ -4,12 +4,13 @@ function timeline() {
     let updatedDates = updateDates();
     let goIns = async function () {
         studySessionData.doneInstructions = "stratIns";
+        platform.saveSession(studySessionData, true);
         let doneInstructions = await startFirstDay();
         if (doneInstructions == "doneInstructions") {
             studySessionData.doneInstructions = "doneInstructions";
             studySessionData.expDaysDate = updatedDates.fullDate;
             studySessionData.startDate = startDate;
-            platform.saveSession(studySessionData, true);
+            platform.saveSession(studySessionData);
         }
     }
     goIns();
