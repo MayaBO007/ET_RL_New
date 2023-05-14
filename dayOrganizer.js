05d1b11302f5eb9849d2fd1b2b855a1afe4272cf
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-05-14 10:54:45 +0300

    ss

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -2,6 +2,7 @@ function moveToDay() {
     platform.getAllSessions().then((data) => {
         getIndexSessionData(data).then((i) => {
             studySessionData = data[i];
+            deleteFromSessionData();
             let updatedDates = updateDates();
             let todayDate = getTodayDate().slice(0, 2);
 
