e81c642cce6e67606d48e515748be95e3236eea3
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-05-15 09:12:02 +0300

    ss

--- days/twoTests/yellowCarInterval.js
+++ days/twoTests/yellowCarInterval.js
@@ -125,9 +125,9 @@ async function startIntervalYellow() {
             clearInterval(sessionIntervalYellow);
             clearTimeout(sessionTimerYellow);
             reset_airplane();
-            reset_yellowCar();
-            reset_blueCar();
-            reset_redCar();
+            // reset_yellowCar();
+            // reset_blueCar();
+            // reset_redCar();
             timeoutCount++;
             if (timeoutCount == 1) {
                 getCarNum().then((carNum) => {
