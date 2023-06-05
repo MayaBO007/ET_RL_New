75ec51fa1743c306df663e27d1be2316f62ea358
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-06-05 12:45:31 +0300

    ss

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -12,7 +12,7 @@ function moveToDay() {
             const problem = document.getElementById("problem");
             const problem_hor = document.getElementById("problem_hor");
 
-            if (todayDate === "22") {
+            if (todayDate === "06") {
                 platform.goToUrl("days/twoTests/twoTests.html");
             } else if (typeof studySessionData === "undefined" || studySessionData.doneInstructions === "") {
                 platform.goToUrl("instructions/instructions.html");
